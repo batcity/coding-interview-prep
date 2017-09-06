deae77e3c807c0228bcede06ecea5f307cd70193
gh045951 <gautham.hullikunte@cerner.com>
2017-09-05 22:38:56 -0500

    Fixed hacky comparison

--- Data-Structures/Java/BinaryTree/BinarySearchTree.java
+++ Data-Structures/Java/BinaryTree/BinarySearchTree.java
@@ -15,8 +15,7 @@ public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E>{
       System.out.println("Element " + value + " found");
       return;
     }
-    //super hacky right here,gotta fix this and add a proper comparator or something
-    else if((Integer)value<(Integer)node.getValue()){
+    else if(value.compareTo(node.getValue())<0){
       search(value,node.getLeft());
     }else{
       search(value,node.getRight());
