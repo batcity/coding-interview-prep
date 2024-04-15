86c1abbfeaa6146af6755e1122b09f402806b270
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2024-04-16 04:04:11 +0530

    Update formatting SlidingWindowMaximum.java

--- sliding-window/java/SlidingWindowMaximum.java
+++ sliding-window/java/SlidingWindowMaximum.java
@@ -4,28 +4,28 @@ import java.util.*;
 class SlidingWindowMaximum {
 	public static int[] findMaxSlidingWindow(int[] nums, int w) {
 
-    int start = 0;
-    int end = 0;
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
-    int[] output = new int[nums.length + 1 - w];
-    int outputIndex = 0;
-
-    while(end < w - 1) {
-      maxHeap.add(nums[end]);
-      end++;
-    }
-
-    while(end < nums.length) {
-      int current = nums[end];
-      maxHeap.add(current);
-      output[outputIndex] = maxHeap.peek();
-      int prev = nums[start];
-      maxHeap.remove(prev);
-      start++;
-      end++;
-      outputIndex++;
-    }
-
-		return output;
+	    int start = 0;
+	    int end = 0;
+	    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+	    int[] output = new int[nums.length + 1 - w];
+	    int outputIndex = 0;
+	
+	    while(end < w - 1) {
+	      maxHeap.add(nums[end]);
+	      end++;
+	    }
+	
+	    while(end < nums.length) {
+	      int current = nums[end];
+	      maxHeap.add(current);
+	      output[outputIndex] = maxHeap.peek();
+	      int prev = nums[start];
+	      maxHeap.remove(prev);
+	      start++;
+	      end++;
+	      outputIndex++;
+	    }
+	
+	    return output;
 	}
 }
