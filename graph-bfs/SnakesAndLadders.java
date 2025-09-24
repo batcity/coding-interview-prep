66717f4b9915abdcaf5bc0e2edfa47f1fc7707e2
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-09-24 22:52:00 +0530

    build out flattened graph for snakes and ladders problem

--- graph-bfs/SnakesAndLadders.java
+++ graph-bfs/SnakesAndLadders.java
@@ -14,15 +14,33 @@ class Solution {
 
         int[] flattenedGraph = new int[n * n];
 
-        int index = start;
-        int current = start;
+        int index = n-1;
+        int flattenedGraphIndex = 0;
+        boolean flip = false;
 
-        while(index < n) {
+        while(index > -1) {
 
-            if(index % 2 == 0) {
-                for()
+            if(flip) {
+                for(int k=n-1; k>-1; k--) {
+                    System.out.println("modifying: " + index + "," + k);
+                    flattenedGraph[flattenedGraphIndex] = board[index][k];
+                    flattenedGraphIndex++;
+                }
+
+                flip = false;
+            } else {
+                for(int c=0; c<n; c++) {
+                    System.out.println("modifying: " + index + "," + c);
+                    flattenedGraph[flattenedGraphIndex] = board[index][c];
+                    flattenedGraphIndex++;
+                }
+                flip = true; 
             }
+               
+            index--;
         }
+
+        // Arrays.stream(flattenedGraph).forEach(x -> System.out.println(x));
 
         return -1;
     }
