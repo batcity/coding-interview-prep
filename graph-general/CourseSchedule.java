9e9d341107a5befe96044d45feeb7855df980198
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-11-12 20:22:47 +0530

    wrap up java implementation for course schedule problem

--- graph-general/CourseSchedule.java
+++ graph-general/CourseSchedule.java
@@ -3,67 +3,52 @@ import java.util.*;
 
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-        // Building the adjacency List for the graph
-        HashMap<Integer, List<Integer>> adjacencyList = new HashMap<>();
-        adjacencyList = buildAdjacencyList(prerequisites);
 
-        HashSet<List<Integer>> visitedEdges = new HashSet<>();
+        HashMap<Integer, List<Integer>> adjacencyList = new HashMap<>();
+        // visited states for each node: 0 = unvisited, 1 = visiting, 2 = done
+        int[] state = new int[numCourses];
+
+        // build adjacencyList
+        for(int[] prerequisite: prerequisites) {
+            adjacencyList.computeIfAbsent(prerequisite[1], v -> new ArrayList<>()).add(prerequisite[0]);
+        }
         
         // dfs through each prerequisite
-        for(int[] prerequisite: prerequisites) {
+        for(int node: adjacencyList.keySet()) {
 
-            int edgeStart = prerequisite[0];
-            int edgeEnd = prerequisite[1];
-            if(visitedEdges.contains(Arrays.asList(edgeStart, edgeEnd))) {
-                continue;
-            } else {
-                HashSet<List<Integer>> tempVisitedEdges = new HashSet<>();
-                System.out.println("going to dfs through " + edgeStart + "," + edgeEnd);
-                boolean hasLoop = dfs(edgeStart, edgeEnd, adjacencyList, tempVisitedEdges);
+            boolean hasCycle = hasCycle(node, adjacencyList, state);
 
-                if(hasLoop) {
-                    return false;
-                }
-
-                visitedEdges.addAll(tempVisitedEdges);
+            if(hasCycle) {
+                return false;
             }
         }
         
         return true;
     }
     
-    private HashMap<Integer, List<Integer>> buildAdjacencyList(int[][] prerequisites) {
-        
-        HashMap<Integer, List<Integer>> adjacencyList = new HashMap<>();
-        
-        for(int[] edge: prerequisites) {
-            if(adjacencyList.containsKey(edge[0])) {
-                adjacencyList.get(edge[0]).add(edge[1]);
-            } else {
-                adjacencyList.put(edge[0], new ArrayList<>(List.of(edge[1])));
-            }
-        }
-        
-        return adjacencyList;
-    }
-    
-    private boolean dfs(int edgeStart, int edgeEnd, HashMap<Integer, List<Integer>> adjacencyList,  HashSet<List<Integer>> visitedEdges) {
-        
-        if(visitedEdges.contains(Arrays.asList(edgeStart, edgeEnd))) {
-            System.out.println("visited " + Arrays.asList(edgeStart, edgeEnd) + " twice in this iteration");
+    private boolean hasCycle(int node, HashMap<Integer, List<Integer>> adjacencyList,  int[] state) {
+
+        if(state[node]==1) {
             return true;
         }
+
+        if(state[node]==2) {
+            return false;
+        }
+
+        // mark node as visiting in this cycle...
+        state[node] = 1;
         
-        visitedEdges.add(Arrays.asList(edgeStart, edgeEnd));
-        
-        if(adjacencyList.containsKey(edgeEnd)) {
-            for(Integer newEdgeEnd: adjacencyList.get(edgeEnd)) {
-                if(dfs(edgeEnd, newEdgeEnd, adjacencyList, visitedEdges) == true) {
+        if(adjacencyList.containsKey(node)) {
+            for(Integer next: adjacencyList.get(node)) {
+                if(hasCycle(next, adjacencyList, state)) {
                     return true;
                 }
             }
         }
+
+        // mark node as visited
+        state[node] = 2;
         
         return false;
     }
