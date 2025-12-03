f6d1ffdf1778b0d9101f43b9cdfed755d4becbe6
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-12-04 01:53:21 +0530

    start work on the single number problem

---
+++ bit-manipulation/single-number-deus/SingleNumberDeus.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/single-number-ii/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        // for example 1, the binary digits look like this
+        // 10
+        // 10
+        // 11
+        // 10
+
+        // I don't see any patterns yet
+        
+    }
+}
