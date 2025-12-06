34d84d1a7d114d68de945a7df2dfef5a1cc81525
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-12-07 02:15:01 +0530

    refine the python solution for construct quad tree problem

--- divide-and-conquer/construct-quad-tree/construct_quad_tree.py
+++ divide-and-conquer/construct-quad-tree/construct_quad_tree.py
@@ -11,22 +11,25 @@ class Node:
 """
 
 class Solution:
+
+    def construct_quad_tree(self, grid: List[List[int]], start, end, size):
+
+        if size == 1:
+            return Node(grid[start][end], True, None, None, None, None)
+
+        topLeft = self.construct_quad_tree(grid, start, start, size/2)
+        topRight = self.construct_quad_tree(grid, start, start + size, size/2)
+        bottomLeft = self.construct_quad_tree(grid, start + size, start, size/2)
+        bottomRight = self.construct_quad_tree(grid, size, size, size/2)
+
+
+        if topLeft.val == topRight.val == bottomlLeft.val == bottomRight.val:
+            return Node(grid[start][end], true, None, None, None, None)
+
+        return Node(grid[start][size], False, topLeft, topRight, bottomLeft, bottomRight)
+
     def construct(self, grid: List[List[int]]) -> 'Node':
 
         start = 0
-        end = grid.length - 1
-        return constructQuadTree(start, end)
-        
-
-    def constructQuadTree(self, grid: List[List[int]], start, end):
-
-        # how do i figure out if the value is just one element
-        # actually each sub quad tree should be a middle point of that tree
-
-        # refine this line
-        Node topLeft = constructQuadTree(self, grid, end/2/2, end/2/2)
-
-        # these start and end pionts are incorrect
-        Node topRight = constructQuadTree(self, grid, start, end)
-        Node bottomLeft = constructQuadTree(self, grid, end, start)
-        Node bottomRight = constructQuadTree(self, grid, end, end)
+        end = len(grid) - 1
+        return self.construct_quad_tree(grid, start, end, len(grid))
