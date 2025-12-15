7d514f70fda6106bde0231266c01bb95d0c30845
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-12-16 02:43:08 +0530

    refine single number problem part two

--- bit-manipulation/single-number-deus/SingleNumberDeus.java
+++ bit-manipulation/single-number-deus/SingleNumberDeus.java
@@ -3,13 +3,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
-        // for example 1, the binary digits look like this
-        // 10
-        // 10
-        // 11
-        // 10
+        int result = 0;
 
-        // I don't see any patterns yet
-        
+        for(int i=0;i<32;i++) {
+
+            int bitSum = 0;
+
+            // get the ith bit from each number
+            // then add them up
+            for(int num: nums) {
+                String bits = String.format("%32s",
+                        Integer.toBinaryString(num)).replace(' ', '0');
+                System.out.println("the 32 bit representation of number: " + num + " is: " + bits);
+
+                // how to get the ith bit in a number
+                // 1. Shift the desired bit to the least significant position (0)
+                // 2. Use the AND operator with 1 (which is 0...001) to isolate that bit
+                bit = (num >> i) & 1;
+            }
+
+        }
+
+        return -1;
     }
 }
