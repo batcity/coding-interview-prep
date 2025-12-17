f48a659bfefe3343af9336f22d5234d6e25059aa
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-12-18 02:04:39 +0530

    refine Single number deus problem

--- bit-manipulation/single-number-deus/SingleNumberDeus.java
+++ bit-manipulation/single-number-deus/SingleNumberDeus.java
@@ -16,10 +16,17 @@ class Solution {
                         Integer.toBinaryString(num)).replace(' ', '0');
                 System.out.println("the 32 bit representation of number: " + num + " is: " + bits);
 
-                // how to get the ith bit in a number
-                // 1. Shift the desired bit to the least significant position (0)
+                // How to get the ith bit in a number:
+                // 1. Shift the desired bit to the least significant position -> this is the rightmost bit in the
+                // binary representation of a number - example: for binary 001 -> 1 is the least significant bit
+                // this is acheived by the num >> i command -> you're essentially shifting the bits in the number num by
+                // i positions, in case the number is positive the new bits that are added to the left are 0, otherwise they are 1
+                //
                 // 2. Use the AND operator with 1 (which is 0...001) to isolate that bit
-                bit = (num >> i) & 1;
+                int bit = (num >> i) & 1;
+
+                System.out.println("ith bit for this number is: " + String.format("%32s",
+                        Integer.toBinaryString(bit)).replace(' ', '0'));
             }
 
         }
