81e311fa009071021e74a889b86437369d782e04
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-12-26 02:19:04 +0530

    add optimal solution for exchange seats sql problem

--- sql/subqueries/exchange_seats.sql
+++ sql/subqueries/exchange_seats.sql
@@ -1,9 +1,8 @@
-# Fix this
-
-UPDATE Seat AS s1
-JOIN Seat AS s2
-  ON (s1.id % 2 = 1 AND s2.id = s1.id + 1)
-  OR (s1.id % 2 = 0 AND s2.id = s1.id - 1)
-SET s1.student = s2.student;
-
-SELECT * FROM Seat;
+Select 
+    CASE
+        WHEN id%2=1 AND id<(select max(id) from seat) then id+1
+        when id%2=0 then id-1
+        else id
+    END AS id,
+    student from Seat
+    order by id
