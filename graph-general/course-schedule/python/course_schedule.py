a368e87359bf4c90a449be56a07cc0548275e814
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-01-03 23:12:56 +0530

    refine courseschedule algorithm

--- graph-general/course-schedule/python/course_schedule.py
+++ graph-general/course-schedule/python/course_schedule.py
@@ -1,22 +1,53 @@
 # https://leetcode.com/problems/course-schedule/?envType=study-plan-v2&envId=top-interview-150
 
+from collections import defaultdict
+
 class Solution:
 
-    def build_adjacency_matrix(self, numCourses: int, prerequisites: List[List[int]]):
+    def build_adjacency_list(self, numCourses: int, prerequisites: List[List[int]]):
 
-        output = [[False for _ in range(numCourses)] for _ in range(numCourses)]
+        prereqs_by_course = defaultdict(list)
 
         for pair in prerequisites:
-            output[pair[1]][pair[0]] = True
 
-        return output
+            course = pair[0]
+            prerequisite = pair[1]
+            prereqs_by_course[course].append(prerequisite)
 
-    def path_exists()
+        return prereqs_by_course
+
+    def does_circle_exist(self, key, values, seen, prereqs_by_course):
+
+        print("seen right now is", seen)
+        if values is None:
+            return False
+
+        if key in seen:
+            print("I've seen this", key)
+            return True
+
+        seen.append(key)
+
+        for value in values:
+            if self.does_circle_exist(value, prereqs_by_course.get(value), seen, prereqs_by_course):
+                return True
+
+        return False
+
+
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        print(self.build_adjacency_matrix(numCourses, prerequisites))
+        
+        # alright my plan is to make an adjacency list that looks like this, prereqs by course
+        # then dfs through each course, if there's a circle then you're out
+        # print(self.build_adjacency_list(numCourses, prerequisites))
 
-        # alright my confusion now is, how shuld the adjacency matrix have been built to navigate this next block
-        # course to prereqs or prereqs to course?
+        prereqs_by_course = self.build_adjacency_list(numCourses, prerequisites)
 
-        path_exists
+        for key, values in prereqs_by_course.items():
+
+            seen = []
+            if self.does_circle_exist(key, values, seen, prereqs_by_course):
+                return False
+
+        return True
