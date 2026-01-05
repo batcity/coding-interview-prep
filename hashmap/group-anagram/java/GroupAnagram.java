9e58e80d551d8f66d409673cf40ac424272f0a0e
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-01-05 23:43:49 +0530

    refine group anagram solution

--- hashmap/group-anagram/java/GroupAnagram.java
+++ hashmap/group-anagram/java/GroupAnagram.java
@@ -1,38 +1,40 @@
 // https://leetcode.com/problems/group-anagrams/?envType=study-plan-v2&envId=top-interview-150
 
+// Time complexity: O(N.K)
+// Space Complexity: O(N · K) due to storing grouped strings and keys
+
 import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
         HashMap<String, List<String>> wordsByCharCount= new HashMap<>();
-        List<List<String>> outputList = new ArrayList<>();
 
         for(String str: strs) {
             String charCountMap = getCharCountMap(str);
-            // System.out.println(charCountMap);
 
             if(wordsByCharCount.containsKey(charCountMap)) {
                 wordsByCharCount.get(charCountMap).add(str);
-
-                // System.out.println("this map exists");
             } else {
                 var newList = new ArrayList<>(List.of(str));
                 wordsByCharCount.put(charCountMap, newList);
-                outputList.add(newList);
             }
         }
 
-        return outputList;
+        return new ArrayList(wordsByCharCount.values());
     }
 
     private String getCharCountMap(String str) {
 
         int[] charCount = new int[26];
+        int size = str.length();
+        int curr = 0;
 
-        for(char c: str.toCharArray()) {
+        while(curr < size) {
+            char c = str.charAt(curr);
             int index = (int) c - 'a';
             charCount[index] = charCount[index] + 1;
+            curr++;
         }
 
         return Arrays.toString(charCount);
