13d61451c64533ac26fbe154ff2ba5c5163f6055
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-01-24 20:20:43 +0530

    optimize python solution for course schedule problem

--- graph-general/course-schedule/python/course_schedule.py
+++ graph-general/course-schedule/python/course_schedule.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/course-schedule/?envType=study-plan-v2&envId=top-interview-150
 
+# Time complexity: O(V + E), where V = numCourses and E = number of prerequisite pairs
+# Space complexity: O(V + E) for the adjacency list, visited sets, and recursion stack
 from collections import defaultdict
 
 class Solution:
@@ -16,21 +18,25 @@ class Solution:
 
         return prereqs_by_course
 
-    def does_circle_exist(self, key, values, seen, prereqs_by_course):
+    def does_circle_exist(self, key, values, visiting, visited, prereqs_by_course):
 
-        print("seen right now is", seen)
+        if key in visited:
+            return False
+
         if values is None:
             return False
 
-        if key in seen:
-            print("I've seen this", key)
+        if key in visiting:
             return True
 
-        seen.append(key)
+        visiting.add(key)
 
         for value in values:
-            if self.does_circle_exist(value, prereqs_by_course.get(value), seen, prereqs_by_course):
+            if self.does_circle_exist(value, prereqs_by_course.get(value), visiting, visited, prereqs_by_course):
                 return True
+
+        visiting.remove(key)
+        visited.add(key)
 
         return False
 
@@ -40,14 +46,13 @@ class Solution:
         
         # alright my plan is to make an adjacency list that looks like this, prereqs by course
         # then dfs through each course, if there's a circle then you're out
-        # print(self.build_adjacency_list(numCourses, prerequisites))
 
         prereqs_by_course = self.build_adjacency_list(numCourses, prerequisites)
+        visited = set()
+        visiting = set()
 
         for key, values in prereqs_by_course.items():
-
-            seen = []
-            if self.does_circle_exist(key, values, seen, prereqs_by_course):
+            if self.does_circle_exist(key, values, visiting, visited, prereqs_by_course):
                 return False
 
         return True
