2db7196188a9f5f3d82ef1898131f42abbf85ac2
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-02-21 19:25:37 +0530

    refine python solution for single number deus problem

--- bit-manipulation/single-number-deus/python/single_number_deus.py
+++ bit-manipulation/single-number-deus/python/single_number_deus.py
@@ -2,8 +2,23 @@
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        for num in nums:
-            print(bin(num)[2:])
 
-            print(num >> 0)
+        for index in range (0,31):
+
+            bit_sum = 0
+            
+            for num in nums:
+
+                print("bin representation of original number: ", num)
+                print(bin(num)[2:])
+
+                print("number after right shifting", index, " bits: ")
+                right_shifted_number = num >> index
+                print(bin(right_shifted_number)[2:])
+
+                # how do I get the ith bit in a number
+                # fairly simple you "AND" everything with one - dang that's amazing
+
+                print("bit", index , "from the number:", num , " is:")
+                bit = right_shifted_number & 1
+                print(bin(bit)[2:])
