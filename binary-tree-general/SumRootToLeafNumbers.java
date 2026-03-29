6ccbabd6db267665a41b1dde3bf85a4240b36f48
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-03-29 19:40:51 +0530

    get started on solution for SumRootToLeafNumbers problem

---
+++ binary-tree-general/SumRootToLeafNumbers.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/sum-root-to-leaf-numbers/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        int total = 0;
+
+        
+    }
+}
