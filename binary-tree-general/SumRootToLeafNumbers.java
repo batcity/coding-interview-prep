cf09b8d93d37b16e563ed4994ed344f5fdb8d25c
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-04-05 20:51:28 +0530

    add optimal solution for sum root to leaf numbers problem

--- binary-tree-general/SumRootToLeafNumbers.java
+++ binary-tree-general/SumRootToLeafNumbers.java
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/sum-root-to-leaf-numbers/?envType=study-plan-v2&envId=top-interview-150
+// Time complexity: O(N) where N is the number of nodes in the tree
+// Space complexity: O(H) where H is the height of the tree (used by the recursion stack)
 
 /**
  * Definition for a binary tree node.
@@ -17,8 +19,21 @@
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
-        int total = 0;
+        return dfs(root, 0);
+    }
 
-        
+
+    public int dfs(TreeNode root, int currentSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        currentSum = currentSum * 10 + root.val;
+
+        if (root.left == null && root.right == null) {
+            return currentSum;
+        } 
+
+        return dfs(root.left, currentSum) + dfs(root.right, currentSum);
     }
 }
