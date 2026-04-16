faaec7664d2fb484bece4a3ddca616319a1d0ae8
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-04-16 20:40:44 +0530

    add solution for managers with atleast 5 reports mysql problem

--- sql/basic-joins/managers_with_atleast_5_direct_reports.sql
+++ sql/basic-joins/managers_with_atleast_5_direct_reports.sql
@@ -1,3 +1,4 @@
 --- leetcode problem: https://leetcode.com/problems/managers-with-at-least-5-direct-reports/?envType=study-plan-v2&envId=top-sql-50
---- TODO: Fix this solution
-select * from Employee e1, Employee e2 where e1.id=e2.managerId
+select e1.name from Employee e1
+join Employee e2 ON e1.id = e2 .managerId 
+group by e1.id having count(*)>4
