68d9c8d75ae04f40ae7d54b46d2faf546ef62d65
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-04-18 19:38:04 +0530

    add final solution for rotate image problem - python

--- matrix/matrix-rotation/rotate_image.py
+++ matrix/matrix-rotation/rotate_image.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
+# Time complexity: O(n*n) since the matrix is n*n
+# Space complexity: O(1) since we're not using any additional space
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
@@ -9,23 +11,23 @@ class Solution:
         # what one needs to do is convert the columns to rows, then reverse the rows
 
         self.convert_columns_to_rows(matrix)
+        self.reverse_list(matrix)
     
     def convert_columns_to_rows(self, matrix: List[List[int]]):
 
+        # The trick is to only swap elements in the upper right triangle
         size = len(matrix)
-        # WAIT, I can't use another matrix
-        # so the trick is to only swap elements in the upper right triangle
-
         x = 0
 
         while(x<size):
-
             y = x + 1
-
             while(y<size):
                 matrix[x][y], matrix[y][x] = matrix[y][x], matrix[x][y]
                 y = y + 1
             
             x = x + 1
 
-        print(matrix)
+    def reverse_list(self, matrix: List[List[int]]):
+
+        for currList in matrix:
+            currList.reverse()
