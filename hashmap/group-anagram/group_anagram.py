eea5ebc836ca3a54686e4040b18e94f3b0784cff
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-05-16 20:03:14 +0530

    get started on python solution for group anagram problem

--- hashmap/group-anagram/group_anagram.py
+++ hashmap/group-anagram/group_anagram.py
@@ -1,12 +1,28 @@
 # https://leetcode.com/problems/group-anagrams/?envType=study-plan-v2&envId=top-interview-150
+# time complexity: O(N * K) where N is the number of words and k is the number of characters in the words
 
 from collections import Counter
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        words_by_character_counts = {}
+
         for word in strs:
-            self.calculate_character_count(word)
+            curr_char_count = self.calculate_character_count(word)
+            if curr_char_count in words_by_character_counts:
+                words_by_character_counts.get(curr_char_count).append(word)
+            else:
+                words_by_character_counts[curr_char_count] = []
+                words_by_character_counts[curr_char_count].append(word)
+
+        return list(words_by_character_counts.values())
 
     def calculate_character_count(self, word: str):
-        # this won't work because the order of the character counts are different
-        print(Counter(word))
+        lst = [0] * 26
+        
+        for letter in word:
+            index = ord(letter) - ord('a')
+            lst[index] += 1
+
+        return tuple(lst)
