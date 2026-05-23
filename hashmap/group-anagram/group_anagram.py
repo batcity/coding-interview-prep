025fd374c4340e52d16a09bf165ed6d09a9ac98c
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-05-23 18:53:59 +0530

    add optimal solution for group anagram problem in python

--- hashmap/group-anagram/group_anagram.py
+++ hashmap/group-anagram/group_anagram.py
@@ -1,20 +1,17 @@
 # https://leetcode.com/problems/group-anagrams/?envType=study-plan-v2&envId=top-interview-150
-# time complexity: O(N * K) where N is the number of words and k is the number of characters in the words
+# Time complexity: O(N * K) where N is the number of words and k is the number of characters in the words
+# Space complexity: O(N * K) where N is the number of words and k is the number of characters in the words
 
 from collections import Counter
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        words_by_character_counts = {}
+        words_by_character_counts = defaultdict(list)
 
         for word in strs:
             curr_char_count = self.calculate_character_count(word)
-            if curr_char_count in words_by_character_counts:
-                words_by_character_counts.get(curr_char_count).append(word)
-            else:
-                words_by_character_counts[curr_char_count] = []
-                words_by_character_counts[curr_char_count].append(word)
+            words_by_character_counts[curr_char_count].append(word)
 
         return list(words_by_character_counts.values())
 
