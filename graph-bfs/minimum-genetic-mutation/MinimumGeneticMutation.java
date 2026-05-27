857c4169b06eff73950f7c2107fec5442c8c585a
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-05-27 19:38:24 +0530

    add working solution to minimum genetic mutation problem

--- graph-bfs/minimum-genetic-mutation/MinimumGeneticMutation.java
+++ graph-bfs/minimum-genetic-mutation/MinimumGeneticMutation.java
@@ -1,4 +1,8 @@
 // https://leetcode.com/problems/minimum-genetic-mutation/?envType=study-plan-v2&envId=top-interview-150
+// Time complexity: (4 ^ N) * N where N is the number of characters in the given startGene
+// Space complexity: (4 ^ N) * N where N is the number of characters in the given startGene
+
+// TODO: looks like time and space complexity is off and BFS might be a better solution to this problem, so optimize this
 
 class Solution {
 
@@ -7,28 +11,30 @@ class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
 
         HashSet<String> bankSet = new HashSet<>();
+        HashSet<String> visited = new HashSet<>();
         for(String gene: bank) bankSet.add(gene);
 
         var startGeneArray = startGene.toCharArray();
         var endGeneArray = endGene.toCharArray();
-        findMinMutation(startGeneArray, endGeneArray, bankSet, 0);
+        findMinMutation(startGeneArray, endGeneArray, bankSet, visited, 0);
 
         if(minGeneticMutations<Integer.MAX_VALUE) return minGeneticMutations;
 
         return -1;
     }
 
-    void findMinMutation(char[] startGeneArray, char[] endGeneArray, HashSet<String> bankSet, int depth) {
+    private void findMinMutation(char[] startGeneArray, char[] endGeneArray, HashSet<String> bankSet, HashSet<String> visited, int depth) {
 
-        System.out.println("curent startGeneArray is: " + Arrays.toString(startGeneArray) + " and depth is " + depth);
-
-        if(depth>0 && !bankSet.contains(Arrays.toString(startGeneArray))) return;
+        String currStartGene = String.valueOf(startGeneArray);
+        if(depth>0 && !bankSet.contains(currStartGene)) return;
 
         if(Arrays.equals(startGeneArray, endGeneArray)) {
-            System.out.println("does this happen");
             if(depth < minGeneticMutations) minGeneticMutations = depth;
             return;
         }
+        
+        if(visited.contains(currStartGene)) return;
+        visited.add(currStartGene);
 
         int index = 0;
         char[] charSet = {'A', 'C', 'G', 'T'};
@@ -38,7 +44,7 @@ class Solution {
             for(char letter: charSet) {
                 char temp = startGeneArray[index];
                 startGeneArray[index] = letter;
-                findMinMutation(startGeneArray, endGeneArray, bankSet, depth+1);
+                findMinMutation(startGeneArray, endGeneArray, bankSet, visited, depth+1);
                 startGeneArray[index] = temp;
             }
 
