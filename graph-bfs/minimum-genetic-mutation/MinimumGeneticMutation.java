6d15db91e0826868e7ff41dd72f1fecb01d03a9b
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-06-03 18:25:28 +0530

    add working bfs solution to minimum genetic mutation problem in java

--- graph-bfs/minimum-genetic-mutation/MinimumGeneticMutation.java
+++ graph-bfs/minimum-genetic-mutation/MinimumGeneticMutation.java
@@ -2,7 +2,7 @@
 // Time complexity: (4 ^ N) * N where N is the number of characters in the given startGene
 // Space complexity: (4 ^ N) * N where N is the number of characters in the given startGene
 
-// TODO: looks like time and space complexity is off, fix that. Also I'm currently working on the BFS solution for this
+// TODO: looks like time and space complexity is off, fix that. Also this bfs solution works but is slower than my original dfs one, so figure out why
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -28,30 +28,40 @@ class Solution {
         Queue<char[]> currQueue = new LinkedList<>();
         currQueue.add(startGeneArray);
         char[] charSet = {'A', 'C', 'G', 'T'};
-        int index = 0;
+        HashSet<String> visited = new HashSet<>();
 
         while(!currQueue.isEmpty()) {
 
             while(currDepthQueueCount>0) {
 
                 char[] currGeneArray = currQueue.poll();
+                if(Arrays.equals(currGeneArray, endGeneArray)) return depth;
 
-                if(Arrays.equals(currGeneArray, endGeneArray)) {
-                    return depth;
-                }
-i
-                for(char letter: charSet) {
-                    char temp = startGeneArray[index];
-                    currGeneArray[index] = letter;
-                    String currStartGene = String.valueOf(startGeneArray);
-                    if(bankSet.contains(currStartGene)) currQueue.add(currGeneArray);
-                    currGeneArray[index] = temp;
+                int index = 0;
+
+                while(index < currGeneArray.length) {
+
+                    for(char letter: charSet) {
+                        char temp = currGeneArray[index];
+                        currGeneArray[index] = letter;
+                        String currStartGene = String.valueOf(currGeneArray);
+                        if(visited.contains(currStartGene)) {
+                            currGeneArray[index] = temp;
+                            continue;
+                        }
+                        if(bankSet.contains(currStartGene)) currQueue.add(currGeneArray.clone());
+                        visited.add(currStartGene);
+                        currGeneArray[index] = temp;
+                    }
+
+                    index++;
                 }
 
                 currDepthQueueCount--;
             }
 
-            currDepthQueueCount = index;
+            currDepthQueueCount = currQueue.size();
+            depth++;
         }
 
         return -1;
