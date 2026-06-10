1887784c8fef181e09dcd6f449bad4538528d5f9
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-06-10 15:20:37 +0530

    add optimal final solution for minimum genetic mutations problem in java

--- graph-bfs/minimum-genetic-mutation/MinimumGeneticMutation.java
+++ graph-bfs/minimum-genetic-mutation/MinimumGeneticMutation.java
@@ -1,61 +1,51 @@
 // https://leetcode.com/problems/minimum-genetic-mutation/?envType=study-plan-v2&envId=top-interview-150
-// Time complexity: (4 ^ N) * N where N is the number of characters in the given startGene
-// Space complexity: (4 ^ N) * N where N is the number of characters in the given startGene
-
-// TODO: looks like time and space complexity is off, fix that. Also make this code cleaner
+// Time complexity: O(B * L^2) where B is the number of elements in the bank and L is the length of the gene string
+// Space complexity: O(B * L) where B is the number of elements in the bank and L is the length of the gene string
 
 import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
 
-    int minGeneticMutations = Integer.MAX_VALUE;
-
     public int minMutation(String startGene, String endGene, String[] bank) {
 
-        HashSet<String> univistedBankSet = new HashSet<>(List.of(bank));
-        return findMinMutation(startGene, endGene, univistedBankSet);
+        HashSet<String> unvistedBankSet = new HashSet<>(List.of(bank));
+        return findMinMutation(startGene, endGene, unvistedBankSet);
     }
 
-    private int findMinMutation(String startGene, String endGene, HashSet<String> univistedBankSet) {
+    private int findMinMutation(String startGene, String endGene, HashSet<String> unvistedBankSet) {
 
         int depth = 0;
-        int currDepthQueueCount = 1;
         Queue<String> currQueue = new LinkedList<>(List.of(startGene));
         char[] charSet = {'A', 'C', 'G', 'T'};
 
-        univistedBankSet.remove(startGene);
+        unvistedBankSet.remove(startGene);
 
         while(!currQueue.isEmpty()) {
 
-            while(currDepthQueueCount>0) {
+            int levelSize = currQueue.size();
+
+            for(int i=0; i<levelSize; i++) {
 
                 String currGene = currQueue.poll();
                 if(currGene.equals(endGene)) return depth;
                 char[] currGeneArray = currGene.toCharArray();
 
-                int index = 0;
-
-                while(index < currGeneArray.length) {
+                for(int index=0; index < currGeneArray.length; index++) {
 
                     for(char letter: charSet) {
                         char temp = currGeneArray[index];
                         currGeneArray[index] = letter;
                         String currStartGene = String.valueOf(currGeneArray);
-                        if (univistedBankSet.contains(currStartGene)) {
+                        if (unvistedBankSet.contains(currStartGene)) {
                             currQueue.add(currStartGene);
-                            univistedBankSet.remove(currStartGene);
+                            unvistedBankSet.remove(currStartGene);
                         }
                         currGeneArray[index] = temp;
                     }
-
-                    index++;
                 }
-
-                currDepthQueueCount--;
             }
 
-            currDepthQueueCount = currQueue.size();
             depth++;
         }
 
